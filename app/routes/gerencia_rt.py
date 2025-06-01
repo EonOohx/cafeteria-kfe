d077fbb2dfd1e74bcb820bda5715a0dffc399e71
EonOohx <RaulAlejandro_RodriguezR@hotmail.com>
2025-05-31 22:05:26 -0600

    Add management files - important

---
+++ app/routes/gerencia_rt.py
@@ -0,0 +1,80 @@
+from datetime import datetime, timedelta
+
+from flask import Blueprint, render_template, request, jsonify
+from sqlalchemy import func
+
+from app.models.producto import Productos
+from app.models.registro_ventas import DetallesVentas
+from app.models.ventas import Ventas
+from app.utils.db import db
+
+gerencia_bp = Blueprint("gerencia", __name__)
+
+
+@gerencia_bp.route("/reportes", methods=["GET"])
+def gerencia(): return render_template("gerencia.html")
+
+
+@gerencia_bp.route("/reportes/populares", methods=["GET"])
+def productos_populares():
+    resultados = (
+        db.session.query(
+            DetallesVentas.id_producto,
+            func.SUM(DetallesVentas.cantidad).label("cantidad"),
+            Productos.nombre
+        )
+        .join(Productos, DetallesVentas.id_producto == Productos.id_producto)
+        .group_by(DetallesVentas.id_producto, Productos.nombre)
+        .order_by(func.SUM(DetallesVentas.cantidad).desc())
+        .limit(3)
+        .all()
+    )
+    return jsonify([registros_productos_to_dict(d) for d in resultados]), 200
+
+
+@gerencia_bp.route("/reportes/fechas", methods=["GET"])
+def productos_fechas():
+    fecha_inicio = request.args.get("inicio")
+    fecha_final = request.args.get("final")
+    if fecha_inicio and fecha_final:
+        return obtener_productos_fecha(fecha_inicio, fecha_final)
+    else:
+        return jsonify({"error": "No se proporcionaron las fechas para la busqueda"}), 400
+
+
+@gerencia_bp.route("/reportes/ventas", methods=["GET"])
+def productos_ventas():
+    resultado = (db.session.query(
+        func.COUNT(DetallesVentas.id_producto).label("ventas"),
+        Productos.nombre)
+                 .join(Productos, DetallesVentas.id_producto == Productos.id_producto)
+                 .group_by(Productos.nombre)
+                 .order_by(Productos.nombre.desc()))
+    return jsonify([registros_ventas_to_dict(d) for d in resultado]), 200
+
+
+def registros_ventas_to_dict(resultado):
+    return {"producto": resultado.nombre, "ventas": resultado.ventas}
+
+
+def obtener_productos_fecha(inicio, final):
+    fecha_inicio = datetime.strptime(inicio, "%Y-%m-%d")
+    fecha_fin = datetime.strptime(final, "%Y-%m-%d") + timedelta(days=1)
+    resultados = (
+        db.session.query(
+            DetallesVentas.id_producto,
+            func.SUM(DetallesVentas.cantidad).label("cantidad"),
+            Productos.nombre
+        )
+        .join(Productos, DetallesVentas.id_producto == Productos.id_producto)
+        .join(Ventas, DetallesVentas.id_venta == Ventas.id_venta)
+        .where(Ventas.fecha.between(fecha_inicio, fecha_fin))
+        .group_by(DetallesVentas.id_producto, Productos.nombre)
+        .order_by(func.SUM(DetallesVentas.cantidad).desc())
+        .all()
+    )
+    return jsonify([registros_productos_to_dict(d) for d in resultados]), 200
+
+
+def registros_productos_to_dict(resultado):
+    return {"id": resultado.id_producto, "nombre": resultado.nombre, "cantidad": resultado.cantidad}
---
+++ app/static/js/gerencia.js
@@ -0,0 +1,121 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("form").addEventListener("submit", async (e) => {
+        e.preventDefault();
+        consultarProductosFechas().catch();
+    });
+
+    document.querySelector("input[name='fecha_inicio']").value = fechaActual();
+    document.querySelector("input[name='fecha_final']").value = fechaActual();
+    consultarProductosFechas().catch();
+    renderizarProductosVendidos().catch();
+    renderizarProductosPopulares().catch();
+});
+
+
+function fechaActual() {
+    const date = new Date()
+    const anio = date.getFullYear();
+    const mes = String(date.getMonth() + 1).padStart(2, "0");
+    const dia = String(date.getDate()).padStart(2, "0");
+    return `${anio}-${mes}-${dia}`;
+}
+
+async function consultarProductosFechas() {
+    const url = document.getElementById("btn-productos-fechas").dataset.url;
+    try {
+        const fecha_inicio = document.querySelector("input[name='fecha_inicio']").value;
+        const fecha_final = document.querySelector("input[name='fecha_final']").value;
+        const response = await fetch(`${url}?inicio=${fecha_inicio}&final=${fecha_final}`);
+        if (!response.ok) {
+            console.log(response.statusText);
+            return;
+        }
+        const registros = await response.json();
+        renderizarProductosRangoFecha(registros);
+    } catch (error) {
+        console.log("No se pudo realizar la consulta de registros entre fechas", error);
+    }
+}
+
+
+function renderizarProductosRangoFecha(registros) {
+    const tbody = document.getElementById("tb-productos-fecha");
+    tbody.innerHTML = "";
+    registros.forEach(registro => {
+        const {id, nombre, cantidad} = registro;
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+            <td>${id}</td>
+            <td>${nombre}</td>
+            <td>${cantidad}</td>
+        `;
+        tbody.appendChild(fila);
+    });
+}
+
+async function renderizarProductosVendidos() {
+    try {
+        const url = document.getElementById("div-productos-venta").dataset.url;
+        const response = await fetch(`${url}`);
+        if (!response.ok) {
+            console.log(response.statusText);
+            return;
+        }
+
+        const registros = await response.json();
+        const etiquetas = registros.map(item => item.producto);
+        const valores = registros.map(item => item.ventas);
+
+        const ctx = document.getElementById("canva-productos-venta").getContext("2d");
+
+        // El objeto Chart funciona al cargarse el DOM.
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: etiquetas,
+                datasets: [{
+                    label: 'Ventas por producto',
+                    data: valores,
+                    backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+    } catch (error) {
+        console.log("No se pudo realizar la consulta de las ventas por productos ", error);
+    }
+}
+
+async function renderizarProductosPopulares() {
+    try {
+        const tbody = document.getElementById("tb-productos-populares");
+        const response = await fetch(`${tbody.dataset.url}`);
+        if (!response.ok) {
+            console.log(response.error);
+            return;
+        }
+        const registros = await response.json();
+        registros.forEach(registro => {
+            const {id, nombre, cantidad} = registro
+            const fila = document.createElement("tr");
+            fila.innerHTML = `
+                <td>${id}</td>
+                <td>${nombre}</td>
+                <td>${cantidad}</td>
+            `;
+            tbody.appendChild(fila);
+        })
+    } catch (error) {
+        console.log("No se pudo realizar la consulta de los productos populares", error);
+    }
+}
